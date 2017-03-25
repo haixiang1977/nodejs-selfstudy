9d369bb55a9c4688065bfffcc95179c8c8b2baa2
liu haixiang <haixiang1977@yahoo.com>
2017-03-25 10:24:01 +0800

    add module example
    Signed-off-by: liu haixiang <haixiang1977@yahoo.com>

---
+++ currencyapp/test-currency.js
@@ -0,0 +1,8 @@
+var currency = require('./currency');
+
+console.log('50 canadian dollars equals to this amount of US dollars: ');
+console.log(currency.canadianToUS(50));
+
+console.log('30 US dollars equals to this amount of Canadian dollars: ');
+console.log(currency.USToCanadian(30));
+
